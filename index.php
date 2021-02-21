35ceb498e1754b17a1ebef61af3744d56ae52439
Jahin <jahin.hsn101@gmail.com>
2021-02-22 01:42:53 +0600

    login and registration pages created

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome - Coder Timer</title>
+</head>
+<body>
+    <div class="loginBox">
+        <form action="">
+            <input type="text" name="LgnuUserInp" placeholder="Enter your username">
+            <input type="password" name="lgnPassInp" placeholder="Enter your password">
+            <button type="submit">LOGIN</button>
+            <p>Don't have a account ? <a href="UserRegister.php">Register</a></p>
+        </form>
+    </div>
+</body>
+</html>
---
+++ userRegister.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CoderTimer - User Registration</title>
+</head>
+<body>
+    <div class="registrationBox">
+        <form action="">
+            <input type="text" name="userNameInp" id="regUser" placeholder="Enter a unique username....">
+            <input type="password" name="passwordInp" id="regPass" placeholder="Enter a password of min 8 digits" >
+            <input type="password" name="passwordConfirmInp" id="regPassCon" placeholder="Retype the password">
+            <input type="text" name="ageInp" id="regAge" placeholder="Enter your age">
+            <input type="email" name="emailInp" id="regMail" placeholder="Enter a valid Email">
+            <button type="submit" >REGISTER</button>
+        </form>
+        <p>Alreay a user ? <a href="index.php">Login here</a></p>
+    </div>
+</body>
+</html>
